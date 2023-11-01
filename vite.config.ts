190eccfd3dc1a1b5e37175ad5040f2a25a4b2cdd
Alexander Milevski <alexander@linkurio.us>
2023-11-01 15:44:27 +0100

    Config, gh pages

---
+++ vite.config.ts
@@ -0,0 +1,18 @@
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  // Set the root directory of your project
+  root: "./",
+  base: "/jewish-flux/",
+
+  // Set the output directory for your built files
+  build: {
+    target: "esnext",
+    outDir: "dist",
+  },
+
+  // Set up your server options
+  server: {
+    port: 3000,
+  },
+});
