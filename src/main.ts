84169dbd7c630bb34fbd857ddd557eb2bfe22be4
Alexander Milevski <alexander@linkurio.us>
2023-11-01 16:37:38 +0100

    Linted

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 import "./style.css";
-import * as d3 from "d3";
+//import * as d3 from "d3";
 import * as Plot from "@observablehq/plot";
 import * as topojson from "topojson";
 import type { FeatureCollection } from "geojson";
